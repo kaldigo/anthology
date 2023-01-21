960a90674f0a576bdcaccb909e78c3dca03450aa
Kaldigo <6437691+kaldigo@users.noreply.github.com>
2023-01-21 18:32:26 +0000

    Show metadata value when multislect value changes

--- Utils/ListUtils.cs
+++ Utils/ListUtils.cs
@@ -14,5 +14,19 @@
             }
             return equal;
         }
+        public static bool CompareLists<T>(List<T> aList, List<T> bList, string compareField)
+        {
+            var equal = true;
+            if (!((aList == null || aList.Count == 0) && (bList == null || bList.Count == 0)))
+            {
+                if (((aList == null || aList.Count == 0) && !(bList == null || bList.Count == 0)) || (!(aList == null || aList.Count == 0) && (bList == null || bList.Count == 0))) return false;
+                var aCompareList = aList.Select(v => typeof(T).GetProperty(compareField).GetValue(v).ToString()).ToList();
+                var bCompareList = bList.Select(v => typeof(T).GetProperty(compareField).GetValue(v).ToString()).ToList();
+                var aEqual = aCompareList.All(a => bCompareList.Any(b => a == b));
+                var bEqual = bCompareList.All(b => aCompareList.Any(a => b == a));
+                equal = aEqual && bEqual;
+            }
+            return equal;
+        }
     }
 }
