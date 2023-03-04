cbb31855e9dc215dcd671db4fde58be3c6566836
Kaldigo <6437691+kaldigo@users.noreply.github.com>
2023-03-04 09:47:40 +0000

    Added option for primary cover

---
+++ Data/Migrations/20230304081315_Added primary book cover field.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Anthology.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedprimarybookcoverfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "PersonImage",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "BookImage",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "BookCover",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPrimary",
+                table: "AudiobookCover",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "PersonImage");
+
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "BookImage");
+
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "BookCover");
+
+            migrationBuilder.DropColumn(
+                name: "IsPrimary",
+                table: "AudiobookCover");
+        }
+    }
+}
