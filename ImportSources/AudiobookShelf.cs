f3c4925caa84d4b507cdf0b11f441a59a58814e1
Kaldigo <6437691+kaldigo@users.noreply.github.com>
2023-09-09 08:13:12 +0100

    Updated ABS import for ABS update

--- ImportSources/AudiobookShelf.cs
+++ ImportSources/AudiobookShelf.cs
@@ -30,7 +30,7 @@ namespace Anthology.Plugins.LibrarySources
                 {
                     using (HttpClient client = new HttpClient())
                     {
-                        var url = settings["Url"] + "/api/libraries/" + settings["LibraryID"] + "/items";
+                        var url = settings["Url"] + "/api/libraries/" + settings["LibraryID"] + "/items?limit=-1";
                         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", settings["Bearer"]);
                         var response = client.GetStringAsync(url).Result;
                         if (response != null)
